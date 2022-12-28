667ccde6690a14ee93e9eb77b8f49b7adb6a7a66
Lucas A. Lisboa <lucasalisboa.ll@gmail.com>
2022-12-28 20:23:26 -0300

    style app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { FiSearch } from 'react-icons/fi';
+import { styles } from './styles.css';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Teste</h1>
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+      <div className="containerInput">
+        <input type="text" placeholder="Digite Aqui..."/>
+          <button className="buttonSearch">
+            <FiSearch size={25} color="#FFF"/>
+          </button>
+      </div>
+
+      <main className='main'>
+        <h2 >CEP: 38050-060</h2>
+
+        <span>Rua Goias </span>
+        <span>Complemento: Algo</span>
+        <span>Santa Maria </span>
+        <span>Uberaba - MG</span>
+        <span></span>
+      </main>
     </div>
   );
 }
